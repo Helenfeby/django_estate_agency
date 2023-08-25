a5a2f2c1b9888e261ca9766ffb4cd6ec1725e6e1
Helenfeby <helenfeby.n@gmail.com>
2023-08-25 14:24:20 +0100

    first commit

---
+++ message/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import MyMessages
+
+# Register your models here.
+class AdminMessage(admin.ModelAdmin):
+    list_display = ('id', 'email', 'name', 'pname', 'plocation', 'msg_date', 'agent_id')
+    list_display_links = ('id', 'email')
+    list_filter = ('agent_id',)
+    search_fields = ('agent_id', 'name')
+    list_per_page  = 25
+
+admin.site.register(MyMessages, AdminMessage)
